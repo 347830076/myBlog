e142e6983a736f74e52935ea5d44de0d9d52eaa1
347830076 <347830076@qq.com>
2020-08-16 23:00:31 +0800

    搭建博客

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    title: '阿离王-前端分享',
+    description: '阿离王-前端分享笔记',
+    themeConfig: {
+        logo: '/assets/img/logo.jpg',
+        head: [
+            ['link', { rel: 'icon', href: '/logo.jpg' }]
+        ],
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'github', link: '/guide/' },
+            {
+                text: 'Languages',
+                items: [
+                    {
+                        text: 'Group1', items: [
+                            { text: 'Home', link: '/' },
+                            { text: 'css', link: '/css/' },
+                        ]
+                    },
+                    {
+                        text: 'Group2', items: [
+                            { text: 'Home', link: '/' },
+                            { text: 'css', link: '/css/' },
+                        ]
+                    }
+                ]
+            },
+            { text: '谷歌搜索', link: 'https://google.com' },
+        ],
+        sidebar: [
+            '',
+            {
+                title: '美丽的css',   // 必要的
+                path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    '/css/'
+                ]
+            },
+        ],
+        lastUpdated: '更新时间', // string | boolean
+    }
+}
