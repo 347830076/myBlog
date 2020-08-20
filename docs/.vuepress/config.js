b0cdb803bd7fa7fdcf6ab7ed4cd0086d57a7eaa1
347830076 <347830076@qq.com>
2020-08-20 23:44:00 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -113,7 +113,7 @@ module.exports = {
                 ]
             },
             {
-                title: '杂类',   // 必要的
+                title: 'API',   // 必要的
                 path: '/others/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
                 collapsable: false, // 可选的, 默认值是 true,
                 sidebarDepth: 1,    // 可选的, 默认值是 1
