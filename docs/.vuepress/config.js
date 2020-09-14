3af6e0d6e2efd4f541d792ff18eeec0a33ff2f47
347830076 <347830076@qq.com>
2020-09-14 20:18:55 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -121,16 +121,7 @@ module.exports = {
                     '/tool/sshkey',
                     '/tool/githook'
                 ]
-            },
-            {
-                title: 'API',   // 必要的
-                path: '/others/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                collapsable: false, // 可选的, 默认值是 true,
-                sidebarDepth: 1,    // 可选的, 默认值是 1
-                children: [
-                    '/others/bluejApi',
-                ]
-            },
+            }
         ],
         lastUpdated: '更新时间', // string | boolean
     }
